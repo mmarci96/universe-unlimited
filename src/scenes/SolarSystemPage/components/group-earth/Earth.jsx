7948478c324d2696fa4ffafbbcec0c6de07619e3
Marci Sarodi <57949298+mmarci96@users.noreply.github.com>
2024-05-29 12:33:53 +0200

    camera lock working from deafult to earth

--- src/scenes/SolarSystemPage/components/group-earth/Earth.jsx
+++ src/scenes/SolarSystemPage/components/group-earth/Earth.jsx
@@ -1,5 +1,5 @@
 import { useTexture } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
+import { useFrame, useThree } from '@react-three/fiber';
 import React, { useCallback, useRef, useEffect, useState } from 'react';
 import Moon from './Moon';
 import IssSpaceStation from './ISS';
@@ -9,6 +9,7 @@ const Earth = React.memo(({ displacementScale }) => {
 	const earthRef = useRef();
 	const earthPositionRef = useRef(new THREE.Vector3(12, 0, 0));
 	const [hovered, hover] = useState(false)
+	const [followingEarth, setFollowingEarth] = useState(false)
 	
 	const [earthTexture, earthNormalMap, earthSpecularMap, earthDisplacementMap, earthEmissiveMap] = useTexture([
 		'/assets/earth_day_small.jpg',
@@ -27,20 +28,42 @@ const Earth = React.memo(({ displacementScale }) => {
 		earthRef.current.position.set(x, 0, z);
 	
 		earthRef.current.rotation.y += 0.002;
-		earthPositionRef.current = earthRef.current.position
 
 	})
+
+	const toggleFollowingEarth = () => {
+		setFollowingEarth(prev => !prev)
+	}
+
 	useEffect(()=>{
 		document.body.style.cursor = hovered ? 'pointer' : 'auto';
 	},[hovered])
 
-	useFrame(({ clock }) => {
-		updateEarthPosition(clock)
+	useFrame(({ clock, camera }) => {
+		updateEarthPosition(clock)		
+		const earthPositionRef = earthRef.current.position
+
+		const cameraTargetPosition = new THREE.Vector3(
+			earthPositionRef.x + 10,
+			earthPositionRef.y + 2,
+			earthPositionRef.z + 5,
+		)
+
+		if(followingEarth){
+			camera.lookAt(earthPositionRef)
+			camera.position.copy(cameraTargetPosition)
+		} else{
+			const originalCameraPosition = new THREE.Vector3(16,8.4,18)
+			const originalCameraTarget = new THREE.Vector3(0,0,0)
+			camera.lookAt(originalCameraTarget);
+			camera.position.copy(originalCameraPosition);
+		} 
+
 	});
 
 	return (
 		<group ref={earthRef} position={[8, 0, 0]}>
-			<mesh receiveShadow castShadow onPointerOver={()=> hover(true)} onPointerOut={()=>hover(false)} >
+			<mesh receiveShadow castShadow onPointerOver={()=> hover(true)} onPointerOut={()=>hover(false)} onClick={()=>toggleFollowingEarth()} >
 				<sphereGeometry args={[1, 32, 32]} />
 				<meshPhongMaterial
 					map={earthTexture}
--- src/scenes/SolarSystemPage/index.jsx
+++ src/scenes/SolarSystemPage/index.jsx
@@ -10,7 +10,7 @@ const SolarSystemPage = () => {
 				<Canvas camera={{ fov: 55, near: 0.1, far: 1000, position: [16, 8.5, 19.5] }} shadows>
 					<color attach='background' args={['black']} />
 
-					<OrbitControls />
+					{/* <OrbitControls /> */}
 					<SolarSystem />
 				</Canvas>
 			</div>
